b6e06a927b13d8a1ba04fb06b33ec771e49b3e5c
sukh sagar <sss.sukhsagar@gmail.com>
2025-04-02 15:25:17 +0200

    Updated version of the data from v0 to vv1

--- cybulde/config_schemas/config_schema.py
+++ cybulde/config_schemas/config_schema.py
@@ -6,7 +6,7 @@ from pydantic.dataclasses import dataclass
 class Config:
     dvc_remote_name: str = "gdrive"
     dvc_remote_url: str = "gdrive://1c8x_dQpCHhsDAfYzmB08ceroW5kt1T-L"
-
+    dvc_raw_data_folder: str = "data/raw"
 
 def setup_config() -> None:
     cs = ConfigStore.instance()
--- cybulde/version_data.py
+++ cybulde/version_data.py
@@ -1,6 +1,6 @@
 from cybulde.config_schemas.config_schema import Config
 from cybulde.utils.config_utils import get_config
-from cybulde.utils.data_utils import initialize_dvc, initialised_dvc_storage
+from cybulde.utils.data_utils import initialize_dvc, initialised_dvc_storage, commit_to_dvc
 from cybulde.utils.utils import get_logger
 
 
@@ -9,6 +9,7 @@ def version_data(config: Config) -> None:
     # logger = get_logger(Path(__file__).name)
     initialize_dvc()
     initialised_dvc_storage(config.dvc_remote_name,config.dvc_remote_url)
+    commit_to_dvc(config.dvc_raw_data_folder, config.dvc_remote_name)
     print(config)
 
 
