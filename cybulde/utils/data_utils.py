5fcba048a7ff8364d5744440cf384397c555b5be
tiketdata-sukh <106135775+tiketdata-sukh@users.noreply.github.com>
2025-04-02 15:02:38 +0530

    added new functionality to initialize dvc

---
+++ cybulde/utils/data_utils.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+from cybulde.utils.utils import run_shell_command, get_logger
+
+DATA_UTILS_LOGGER = get_logger(Path(__file__).name)
+
+def is_dvc_initialized()->bool:
+    return (Path().cwd() / ".dvc").exists()
+
+def initialize_dvc()->None:
+    if is_dvc_initialized():
+        DATA_UTILS_LOGGER.info("DVC is already initialised")
+        return
+    run_shell_command("dvc init")
+    run_shell_command("dvc config core.analytics false")
+    run_shell_command("dvc config core.autostage true")
+    run_shell_command("git add .dvc")
+    run_shell_command("git commit -nm 'Initialized DVC'")
---
+++ cybulde/version_data.py
@@ -0,0 +1,15 @@
+from cybulde.config_schemas.config_schema import Config
+from cybulde.utils.config_utils import get_config
+from cybulde.utils.data_utils import initialize_dvc
+from cybulde.utils.utils import get_logger
+
+
+@get_config(config_path="../configs", config_name="config")
+def version_data(config: Config) -> None:
+    logger = get_logger(Path(__file__).name)
+    initialize_dvc()
+    print(config)
+
+
+if __name__ == "__main__":
+    version_data()  # type: ignore
